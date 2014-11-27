b40ba842ce096a0d6c8d98044426e27b721f2e47
Geramy <geramy92@users.noreply.github.com>
2014-11-27 11:30:11 +0100

    remove Copyright because of CC-Licence

--- SYSTEM/clicksys_client.lua
+++ SYSTEM/clicksys_client.lua
@@ -1,10 +1,3 @@
---
---	Dieses Script gehört Colin Weber.
---	Dieses Script ist geistiges Eigentum, d.h. es darf weder kopiert, bearbeitet oder verfälscht werden.
---	Es darf nur auf TT-rl.de (TerraTex Reallife) genutzt werden.
---	
---	(c) 2011-2012 by [TTeam]Geramy92 / Colin Weber
---
 addEvent("bindclicksys_event",true)
 
 function rebind_func()
